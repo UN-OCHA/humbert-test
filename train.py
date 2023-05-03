aef122a9f66ed9b457bca565ef14cf0fe9905627
Peter Droogmans (attiks) <peter@attiks.com>
2023-05-03 16:47:46 +0200

    chore: Increase max length

--- train.py
+++ train.py
@@ -7,4 +7,9 @@ train_df = pd.read_csv('training.csv')
 val_df = pd.read_csv('validation.csv')
 architecture = 'base_architecture'
 
-trained_model = trainer.train_classification_model(train_df, val_df, architecture)
+trained_model = trainer.train_classification_model(
+    train_df=train_df,
+    val_df=val_df,
+    architecture_setup=architecture,
+    max_len=500
+)
